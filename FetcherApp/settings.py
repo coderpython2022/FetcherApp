526294308a34ff826ff2d20b0f4dec42622d5f3d
alfath-company <alfathc742@gmail.com>
2024-06-07 02:23:13 +0300

    add DATABASE_URL

--- FetcherApp/settings.py
+++ FetcherApp/settings.py
@@ -106,15 +106,18 @@ WSGI_APPLICATION = 'FetcherApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = "postgresql://postgres:gAffjVocrJGjNPrvTVaLIDlVlKHUlknc@roundhouse.proxy.rlwy.net:34827/railway"
+
 if IS_HEROKU_APP:
     # In production on Heroku the database configuration is derived from the `DATABASE_URL`
     # environment variable by the dj-database-url package. `DATABASE_URL` will be set
     # automatically by Heroku when a database addon is attached to your Heroku app. See:
     # https://devcenter.heroku.com/articles/provisioning-heroku-postgres#application-config-vars
     # https://github.com/jazzband/dj-database-url
+    
     DATABASES = {
         "default": dj_database_url.config(
-            env="DATABASE_URL",
+            env=DATABASE_URL,
             conn_max_age=600,
             conn_health_checks=True,
             ssl_require=True,
