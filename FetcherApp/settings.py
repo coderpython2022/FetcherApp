c3109a89a002dce4698d5bd1b00239e898c30e30
alfath-company <alfathc742@gmail.com>
2024-06-08 02:45:22 +0300

    STATICFILES_DIRS

--- FetcherApp/settings.py
+++ FetcherApp/settings.py
@@ -173,8 +173,10 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STORAGES = {
