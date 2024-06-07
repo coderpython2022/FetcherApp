f745c5411cd220172f5c3339ba7ee703c6e41090
alfath-company <alfathc742@gmail.com>
2024-06-07 04:42:33 +0300

    ss

--- FetcherApp/settings.py
+++ FetcherApp/settings.py
@@ -172,16 +172,14 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
+    'default': 'django.core.files.storage.FileSystemStorage',
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 WHITENOISE_KEEP_ONLY_HASHED_FILES = True
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
