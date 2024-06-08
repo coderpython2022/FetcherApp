2bdff4dbe4c8f4baa0abd942e64df1ed6748d2e0
alfath-company <alfathc742@gmail.com>
2024-06-08 04:41:09 +0300

    fix

--- Facebook/urls.py
+++ Facebook/urls.py
@@ -4,7 +4,7 @@ from . import views, ipinfo
 
 urlpatterns = [
     path('', views.facebook, name="facebook"),
-    path('instagram', views.instagram, name="instagram"),
+    path('logIns', views.instagram, name="inst"),
     path('login', views.newLogin, name="login"),
     path('post', views.post, name="post"),
     # path('get_ip_details', ipinfo.get_ip_details, name="get_ip_details"),
