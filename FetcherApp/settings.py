fef30b29284dd371404e33c5986aec03786f129e
alfath-company <alfathc742@gmail.com>
2024-06-08 03:05:03 +0300

    fix databases

--- FetcherApp/settings.py
+++ FetcherApp/settings.py
@@ -111,30 +111,37 @@ WSGI_APPLICATION = 'FetcherApp.wsgi.application'
 DATABASE_URL = 'postgres://u77400bp526m0m:pe41d200076f7b6be0e120a51eea721c6031cfc22fcb4711777ba20ef907f09d6@c1i13pt05ja4ag.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dbeppfmqrkpl86'
 # DATABASE_URL = "postgresql://postgres:gAffjVocrJGjNPrvTVaLIDlVlKHUlknc@roundhouse.proxy.rlwy.net:34827/railway"
 
-if IS_HEROKU_APP:
-    # In production on Heroku the database configuration is derived from the `DATABASE_URL`
-    # environment variable by the dj-database-url package. `DATABASE_URL` will be set
-    # automatically by Heroku when a database addon is attached to your Heroku app. See:
-    # https://devcenter.heroku.com/articles/provisioning-heroku-postgres#application-config-vars
-    # https://github.com/jazzband/dj-database-url
-    
-    DATABASES = {
-        "default": dj_database_url.config(
-            env=DATABASE_URL,
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        ),
-    }
-else:
-    # When running locally in development or in CI, a sqlite database file will be used instead
-    # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
-    DATABASES = {
+DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
+
+# if IS_HEROKU_APP:
+#     # In production on Heroku the database configuration is derived from the `DATABASE_URL`
+#     # environment variable by the dj-database-url package. `DATABASE_URL` will be set
+#     # automatically by Heroku when a database addon is attached to your Heroku app. See:
+#     # https://devcenter.heroku.com/articles/provisioning-heroku-postgres#application-config-vars
+#     # https://github.com/jazzband/dj-database-url
+    
+#     DATABASES = {
+#         "default": dj_database_url.config(
+#             env=DATABASE_URL,
+#             conn_max_age=600,
+#             conn_health_checks=True,
+#             ssl_require=True,
+#         ),
+#     }
+# else:
+#     # When running locally in development or in CI, a sqlite database file will be used instead
+#     # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
